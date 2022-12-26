d25c5262ac512127c027b29fa18c54c28e2db18f
muhamadalwanfaiz <muhamadalwanfaiz@gmail.com>
2022-12-26 07:12:10 +0700

    add data pasien

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -20,13 +20,57 @@ class AdminController extends Controller
         return view('home', compact('user'));
     }
 
+    // VIEW DATA PASIEN
     public function pasiens()
     {
         $user = Auth::user();
         $pasiens = Pasien::all();
-        return view('pasien',compact('user','pasiens'));
+
+        // Kode Pasien Otomatis
+        $cek = Pasien::count();
+        if ($cek == 0) {
+            $urut = 101;
+            $nomer = 'KP' . $urut;
+            // dd($nomer);
+        } else {
+            $ambil = Pasien::all()->last();
+            $urut = (int)substr($ambil->kodePasien, -3) + 1;
+            $nomer = 'KP' . $urut;
+        }
+
+        return view('pasien',compact('user', 'pasiens', 'nomer'));
     }
 
+    // ADD DATA PASIEN
+    public function submit_pasien(Request $req)
+    {
+        $validate = $req->validate([
+            'kodePasien' => 'required',
+            'nama' => 'required',
+            'gender' => 'required',
+            'umur' => 'required',
+            'alamat' => 'required',
+            'noHp' => 'required',
+        ]);
+
+        $pasien = new Pasien;
+        $pasien->kodePasien = $req->get('kodePasien');
+        $pasien->nama = $req->get('nama');
+        $pasien->gender = $req->get('gender');
+        $pasien->umur = $req->get('umur');
+        $pasien->alamat = $req->get('alamat');
+        $pasien->noHp = $req->get('noHp');
+
+        $pasien->save();
+        $notification = array(
+            'message' => 'Data pasien berhasil ditambahkan',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('admin.pasiens')->with($notification);
+    }
+
+    // VIEW DATA DOKTER
     public function dokters()
     {
         $user = Auth::user();
